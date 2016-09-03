fbe37d5527c134a96369f2dc5a9826f9c749f906
Waqid <wmv214@nyu.edu>
2016-09-03 18:31:09 -0400

    async mocha

--- test/tool-spec.js
+++ test/tool-spec.js
@@ -1,13 +1,30 @@
 var expect = require("chai").expect;
 var tools = require("../lib/tools");
-describe("calculateAge()", function(){
 
-	it("shoud print the name and age of the person", function(){
 
-		var results = tools.calculateAge({ name: "Waqid", yearBorn: 1988 });
+describe("Tools", function(){
 
-		expect(results).to.equal("Waqid is 25 years old");
-
+	describe("calculateAge()", function(){
+		it("shoud print the name and age of the person", function(){
+			var results = tools.calculateAge({ name: "Waqid", yearBorn: 1988 });
+			expect(results).to.equal("Waqid is 28 years old");
+		});
 	});
 
-})
+
+	describe("loadOrgWiki()", function(){
+		it("load Facebook's wiki page", function(done){
+			tools.loadOrgWiki({name: "Facebook"}, function(html){
+				expect(html).to.be.ok;
+				done();
+			});
+		});
+	});
+
+
+
+});
+
+
+
+
