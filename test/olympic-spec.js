f73465a0205c3f2edb5d0ab36a73bc6376780bdd
Waqid <wmv214@nyu.edu>
2016-09-04 02:54:07 -0400

    Using Sinon spy

--- test/olympic-spec.js
+++ test/olympic-spec.js
@@ -3,6 +3,8 @@ var expect = require("chai").expect;
 var rewire = require("rewire");
 var olympics = rewire("../lib/olympics");
 
+var sinon = require("sinon");
+
 describe("Olympics", function(){
 
 	beforeEach(function(){
@@ -16,14 +18,25 @@ describe("Olympics", function(){
 			}
 		];
 
-		olympics.__set__("olympicsData", this.testData);	
+		this.console = {
+			log: sinon.spy(),
+		};
 
+		olympics.__set__("olympicsData", this.testData);	
+		olympics.__set__("console", this.console)
 	});
 
 	describe("getMedalCount()", function(){
-		it("shoud print the total medal count", function(){
-			var results = olympics.getMedalCount({ name: "India"});
-			expect(results).to.equal("India won 2 medals");
+		it("shoud print the total medal count", function(done){
+			var _this = this;
+			olympics.getMedalCount({ name: "India"}, function(results){
+
+				expect(_this.console.log.callCount).to.equal(1);
+				// expect(results).to.equal("India won 2 medals");
+				done();
+
+			});
+			
 		});
 	});
 
